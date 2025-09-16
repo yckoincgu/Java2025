2baf19e05805ca8cfcc896731918065de33c40fd
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2025-09-17 05:27:26 +0800

    20250917_2
    20250917_2

---
+++ J01_Hello.java
@@ -0,0 +1,8 @@
+public class J01_Hello {
+    // Main.java (JDK 24 preview features may require flags)
+
+    public static void main(String[] args) {
+        System.out.println("Running on JDK " + Runtime.version());
+    }
+
+}
